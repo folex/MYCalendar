bef8be1334af16227258c4f1bec745d2c5bc249f
folex <0xdxdy@gmail.com>
2012-07-10 16:41:09 +0400

    created project

---
+++ MYCalendarTests/MYCalendarTests.h
@@ -0,0 +1,13 @@
+//
+//  MYCalendarTests.h
+//  MYCalendarTests
+//
+//  Created by Alexey on 10.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MYCalendarTests : SenTestCase
+
+@end
---
+++ MYCalendarTests/MYCalendarTests.m
@@ -0,0 +1,32 @@
+//
+//  MYCalendarTests.m
+//  MYCalendarTests
+//
+//  Created by Alexey on 10.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MYCalendarTests.h"
+
+@implementation MYCalendarTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MYCalendarTests");
+}
+
+@end
